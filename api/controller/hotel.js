b1e7e51bc1499c7271721889eabae594feccd935
shoukatrai <raishoukat033@gmail.com>
2025-07-12 01:05:29 +0500

    132min

--- api/controller/hotel.js
+++ api/controller/hotel.js
@@ -72,14 +72,51 @@ export const getHotels = async (request, response, next) => {
   }
 };
 
-export const countByCity = async (request, response, next) => {
+export const countByCity = async (req, res, next) => {
+  const cities = req.query.cities.split(",");
   try {
-    const hotels = await Hotel.find();
-    response.status(200).json({
-        status :true,
-        data : hotels
-    });
-  } catch (error) {
-    next(error.message);
+    const list = await Promise.all(
+      cities.map((city) => {
+        return Hotel.countDocuments({ city: city });
+      })
+    );
+    res.status(200).json(list);
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+export const countByType = async (req, res, next) => {
+  try {
+    const hotelCount = await Hotel.countDocuments({ type: "hotel" });
+    const apartmentCount = await Hotel.countDocuments({ type: "apartment" });
+    const resortCount = await Hotel.countDocuments({ type: "resort" });
+    const villaCount = await Hotel.countDocuments({ type: "villa" });
+    const cabinCount = await Hotel.countDocuments({ type: "cabin" });
+
+    res.status(200).json([
+      { type: "hotel", count: hotelCount },
+      { type: "apartments", count: apartmentCount },
+      { type: "resorts", count: resortCount },
+      { type: "villas", count: villaCount },
+      { type: "cabins", count: cabinCount },
+    ]);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getHotelRooms = async (req, res, next) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    const list = await Promise.all(
+      hotel.rooms.map((room) => {
+        return Room.findById(room);
+      })
+    );
+    res.status(200).json(list)
+  } catch (err) {
+    next(err);
   }
 };
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,20 +1,33 @@
 import express from "express";
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controller/hotel.js";
-import { verifyAdmin } from "../utils/verifyToken.js";
+import {
+  countByCity,
+  countByType,
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotelRooms,
+  getHotels,
+  updateHotel,
+} from "../controller/hotel.js";
+import Hotel from "../models/Hotel.js";
+import {verifyAdmin} from "../utils/verifyToken.js"
 const router = express.Router();
 
 //CREATE
-router.post("/",verifyAdmin ,createHotel);
-//DELETE
-router.delete("/:id", verifyAdmin,deleteHotel);
-//UPDATE
-router.put("/:id", verifyAdmin,updateHotel );
-//GET
-router.get("/:id", getHotel );
-//GET ALL
-router.get("/", getHotels);
-router.get("/countByCity", getHotels);
-router.get("/countByType", getHotels);
+router.post("/", verifyAdmin, createHotel);
 
+//UPDATE
+router.put("/:id", verifyAdmin, updateHotel);
+//DELETE
+router.delete("/:id", verifyAdmin, deleteHotel);
+//GET
+
+router.get("/find/:id", getHotel);
+//GET ALL
+
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
 
 export default router;
